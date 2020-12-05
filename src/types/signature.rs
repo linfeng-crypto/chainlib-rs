fa702f967473bb546a87f625ae3dd39a14306861
linfeng <linfeng@crypto.com>
2020-12-05 16:18:53 +0800

    Problem: (fix #7) add example to run

--- src/types/signature.rs
+++ src/types/signature.rs
@@ -1,10 +1,10 @@
-use crate::types::key::PublicKey;
+use crate::types::key::PublicKeyWrap;
 use serde::Serialize;
 
 #[derive(Serialize, Debug, Clone, PartialEq, Eq)]
 pub struct Signature {
     pub signature: String,
-    pub pub_key: PublicKey,
+    pub pub_key: PublicKeyWrap,
     pub account_number: u64,
     pub sequence: u64,
 }
